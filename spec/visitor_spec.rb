a7eaab69cfafe0d5920f8baae55ae2e5c969557e
Reid Miller <reid.s.miller@gmail.com>
2023-02-27 09:12:00 -0700

    test: Test for add preferences

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe Visitor do
+  before(:each) do
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+  end
   describe '#Iteration 1' do
     it 'can initialize and convert money into integer' do
-      visitor1 = Visitor.new('Bruce', 54, '$10')
 
-      expect(visitor1.name).to eq('Bruce')
-      expect(visitor1.height).to eq(54)
-      expect(visitor1.spending_money).to eq(10)
-      expect(visitor1.preferences).to eq([])
+      expect(@visitor1.name).to eq('Bruce')
+      expect(@visitor1.height).to eq(54)
+      expect(@visitor1.spending_money).to eq(10)
+      expect(@visitor1.preferences).to eq([])
+    end
+
+    it 'can add preferences for rides' do
+      @visitor1.add_preference(:gentle)
+      @visitor1.add_preference(:water)
+
+      expect(@visitor1.preferences).to eq([:gentle, :water])
     end
   end
 end
