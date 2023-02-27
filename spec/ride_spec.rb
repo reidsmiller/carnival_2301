26271c60629efcfbca6e3295fd0660e96066c959
Reid Miller <reid.s.miller@gmail.com>
2023-02-27 09:35:16 -0700

    test: Add test for boarding riders

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 RSpec.describe Ride do
   before(:each) do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
   end
+  
   describe '#Iteration 2' do
     it 'can instatiate a ride' do
       expect(@ride1.name).to eq('Carousel')
@@ -11,6 +17,16 @@ RSpec.describe Ride do
       expect(@ride1.admission_fee).to eq(1)
       expect(@ride1.excitement).to eq(:gentle)
       expect(@ride1.total_revenue).to eq(0)
+    end
+
+    it 'can board riders' do
+      expect(@ride1.rider_log).to eq([])
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+      expect(@ride1.rider_log).to eq({@visitor1=>2, @visitor2=>1})
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
     end
   end
 end
