3bf7c592a78ceea8ac3c3746e0a06de95a497d04
Reid Miller <reid.s.miller@gmail.com>
2023-02-27 09:26:10 -0700

    test: Add ride instantiation test

---
+++ spec/ride_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe Ride do
+  before(:each) do
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+  end
+  describe '#Iteration 2' do
+    it 'can instatiate a ride' do
+      expect(@ride1.name).to eq('Carousel')
+      expect(@ride1.min_height).to eq(24)
+      expect(@ride1.admission_fee).to eq(1)
+      expect(@ride1.excitement).to eq(:gentle)
+      expect(@ride1.total_revenue).to eq(0)
+    end
+  end
+end
