4607fba379b2aad788ca62c3cdb2bd78059d0910
Reid Miller <reid.s.miller@gmail.com>
2023-02-27 09:14:58 -0700

    test: Add test for tall enough

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Visitor do
   before(:each) do
     @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
   describe '#Iteration 1' do
     it 'can initialize and convert money into integer' do
@@ -18,6 +20,13 @@ RSpec.describe Visitor do
       @visitor1.add_preference(:water)
 
       expect(@visitor1.preferences).to eq([:gentle, :water])
+    end
+
+    it 'can test for tall enough on 54 inch ride' do
+      expect(@visitor1.tall_enough(54)).to be true
+      expect(@visitor2.tall_enough(54)).to be false
+      expect(@visitor3.tall_enough(54)).to be true
+      expect(@visitor1.tall_enough(64)).to be false
     end
   end
 end
