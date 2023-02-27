2eebc716bf6b1ac9207e6e5502224511ab8aa325
Reid Miller <reid.s.miller@gmail.com>
2023-02-27 10:12:57 -0700

    test: Add carnival instantiation test

---
+++ spec/carnival_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+RSpec.describe Carnival do
+  before(:each) do
+    @carival = Carnival.new('Boulder County Fair')
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
+  end
+
+  describe '#iteration 3 - Carnival' do
+    it 'can instantiate new class' do
+      expect(@carnival.name).to eq('Boulder County Fair')
+    end
+
+    
+  end
+end
